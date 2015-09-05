7aa7952c3de2429ab385e31437102d2f4a7b640a
Jonathan <jonathang_25@hotmail.com>
2015-09-05 11:10:46 -0430

    punto16

--- db/migrate/20150723032905_fieldstotrucks.rb
+++ db/migrate/20150723032905_fieldstotrucks.rb
@@ -2,7 +2,7 @@ class Fieldstotrucks < ActiveRecord::Migration
   def change
 
     add_column :trucks, :pesobruto, :string
-    add_column :trucks, :referecia_id, :integer
+    add_column :trucks, :referencia_id, :integer
     add_column :trucks, :marcacapa, :string
 
     add_column :trucks, :tipotrailer, :string
