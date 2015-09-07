fe82f722ab3e4c7caff19a6aa3ce7d2b9fccb1f9
Jonathan <jonathang_25@hotmail.com>
2015-09-07 15:10:06 -0430

    se mejoran las rutas en el motor de busqueda

--- app/controllers/concerns/toggle_searches.rb
+++ app/controllers/concerns/toggle_searches.rb
@@ -14,10 +14,9 @@ module ToggleSearches
   def read_toggle option
     puts "******#{session[:toggle_search]}**************"
     if option
-      new_hash = eval(option)
-      puts "*******************#{new_hash.keys[0]}*************************"
+      puts "*******************#{option}*************************"
       session[:toggle_search] = eval(session[:toggle_search]) if session[:toggle_search]["q"].class.to_s == 'String'
-      session[:toggle_search]['q'][new_hash.keys[0]] = ""
+      session[:toggle_search]['q'][option] = ""
     end
     puts "******#{session[:toggle_search]["q"]}**************" 
   end
@@ -36,7 +35,7 @@ module ToggleSearches
         search                      = Array.new
         search[0]                   = BrandTruck.select("id,name").find(query["brand_truck_id_eq"]).name
         query["brand_truck_id_eq"] = ""
-        search[1]                   = {"brand_truck_id_eq" => ""}
+        search[1]                   = "brand_truck_id_eq"
         array_searches << search
       end
 
@@ -45,25 +44,25 @@ module ToggleSearches
       unless query["type_truck_id_eq"].blank?
         search     = Array.new
         search[0]  = TypeTruck.select("id,name").find(query["type_truck_id_eq"]).name
-        search[1]  = {"type_truck_id_eq" => ""}
+        search[1]  = "type_truck_id_eq"
         array_searches << search
       end
       unless query["brand_extra_id_eq"].blank?
         search     = Array.new
         search[0]  = BrandExtra.select("id,name").find(query["brand_extra_id_eq"]).name
-        search[1]  = {"brand_extra_id_eq" => ""}
+        search[1]  = "brand_extra_id_eq"
         array_searches << search
       end
       unless query["price_gteq"].blank?
         search     = Array.new
         search[0]  = "precio 1: #{number_to_currency(query['price_gteq'], precision: 0)}"
-        search[1]  = {"price_gteq" => ""}
+        search[1]  = "price_gteq"
         array_searches << search
       end
       unless query["price_lteq"].blank?
         search     = Array.new
         search[0]  = "precio 2: #{number_to_currency(query['price_lteq'], precision: 0)}"
-        search[1]  = {"price_lteq" => ""}
+        search[1]  = "price_lteq"
         array_searches << search
       end
       
@@ -72,7 +71,7 @@ module ToggleSearches
         search                      = Array.new
         search[0]                   = TypeService.select("id,name").find(query["type_service_id_eq"]).name
         query["type_service_id_eq"] = ""
-        search[1]                   = {"type_service_id_eq" => ""}
+        search[1]                   = "type_service_id_eq"
         array_searches << search
       end
 
@@ -81,13 +80,13 @@ module ToggleSearches
         search               = Array.new
         search[0]            =  State.select("id,name").find(query["state_id_eq"]).name
         query["state_id_eq"] = ""
-        search[1]            = {"state_id_eq" => ""}
+        search[1]            = "state_id_eq" 
         array_searches << search
       end
       unless query["name_cont"].blank?
         search     = Array.new
         search[0]  = query["name_cont"]
-        search[1]  = {"name_cont" => ""}
+        search[1]  = "name_cont"
         array_searches << search
       end
     end
