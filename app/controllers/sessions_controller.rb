52f6cc7c9808727e2b2758ade2bb6a183bba6e11
Jonathan <jonathang_25@hotmail.com>
2015-09-12 23:18:06 -0430

    se termina el proceso de creacion de clientes gratuitos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,18 +13,25 @@ class SessionsController < ApplicationController
   def crear_usuario
      @cliente        = Customer.new customer_params
      @cliente.estado = Environment::STATUS[:clientes][:activo]
-     #if @cliente.save
-     #  session[:user] = @cliente.id
-     #  redirect_to micuenta_path
-     #else
-     #  render :registrar_usuario
-     #end
+     free_plan       = Offer.find_by(precio1: 0, precio2: 0)          
+     if free_plan
+       if @cliente.save
+         Offercustomer.create(customer_id: @cliente.id, offer_id: free_plan.id)
+         session[:user] = @cliente.id
+         redirect_to micuenta_path
+       else
+         render :registrar_usuario
+       end
+     else
+       flash[:error] = "No hay planes gratuitos por favor comunicarse con el administrador del sistema"
+       render :registrar_usuario
+     end
   end
 
   private
 
     def customer_params
-      params.require(:customer).permit(:cedula, :name, :email, :clave, :confirm_clave)
+      params.require(:customer).permit(:cedula, :name, :telefono, :email, :clave, :clave_confirmation)
     end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,28 @@
 class Customer < ActiveRecord::Base
 
-  attr_accessor :confirm_clave
+  attr_accessor :clave_confirmation
   
   has_many :offercustomers
   has_many :offer, through: :offercustomers
-
-
   has_many :extras
   has_many :trucks
   has_many :services
+
+  validates_presence_of       :cedula,   message: "El documento de identidad es un campo obligatorio"
+  validates_presence_of       :name,     message: "El nombre es un campo obligatorio"
+  validates_presence_of       :telefono, message: "El telefono es un campo obligatorio"
+  validates_presence_of       :email,    message: "El correo es un campo obligatorio"
+  validates_confirmation_of   :clave,    message: "Las contraseñas no son iguales"
+
+  validates :clave_confirmation, presence: true
+  validates :name, length:     { minimum: 5,  maximum: 50 ,   message: "El nombre debe tener minimo 5 y maximo 50 caracteres" }
+  validates :telefono, length: { minimum: 13,  maximum: 14 ,   message: "El formato indicado del telefono (xxx)-xxx-xxxx" }
+  validates :cedula, length:   { maximum: 13 ,  message: "El documento de identidad debe tener minimo maximo 13 caracteres" }
+  validates :clave, length:    { in: 6..10 ,   message: "La contraseña debe tener minimo 6 y maximo 10 caracteres" }
+
+  validates_uniqueness_of     :email,    message: "El correo electronico ya esta registrado"
+  validates_uniqueness_of     :cedula,   message: "El documento de identidad  ya esta registrado"
+  
+
 
 end
