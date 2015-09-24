ba864057a200f6ace7e3b6f20f57c961fafaef5c
Jonathan <jonathang_25@hotmail.com>
2015-09-23 22:53:18 -0430

    se agrega el message id

--- app/mailers/customer/customer_mailer.rb
+++ app/mailers/customer/customer_mailer.rb
@@ -1,6 +1,6 @@
 class Customer::CustomerMailer < ActionMailer::Base
   default from: "tucamionsoporte@gmail.com"
-
+  default "Message-ID"=>"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com>"
   def create_by_admin(user, url)
     attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
   	@user    = user
