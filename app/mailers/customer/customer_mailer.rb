1ab3976eff429dffb2f29835e95d3374a9785810
jonathan rojas <jrojas@nuclearagenciadigital.com>
2015-09-24 15:42:23 -0430

    se agrega la cabecera

--- app/mailers/customer/customer_mailer.rb
+++ app/mailers/customer/customer_mailer.rb
@@ -3,9 +3,10 @@ class Customer::CustomerMailer < ActionMailer::Base
 
   def create_by_admin(user, url)
     attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
-  	@user    = user
-  	@url     = url
-  	@subject = 'Bienvenido a tucamion365'
+    @user                          = user
+    @url                           = url
+    @subject                       = 'Bienvenido a tucamion365'
+    headers['Message-ID']          = "<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com>"
     mail to: ["#{@user.name} <#{@user.email}>"], subject: @subject
   end
 
@@ -15,9 +16,10 @@ class Customer::CustomerMailer < ActionMailer::Base
 
   def forgot_pass(user, url)
     attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
-    @user    = user
-    @url     = url
-    @subject = 'NOTIFICACION Tucamion365'
+    @user                          = user
+    @url                           = url
+    @subject                       = 'NOTIFICACION Tucamion365'
+    headers['Message-ID']          = "<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com>"
     mail to: ["#{@user.name} <#{@user.email}>"], subject: @subject    
   end
 
