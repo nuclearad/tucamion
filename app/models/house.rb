7858a650b67fd9273c8bc7fe0b7a782094f13fe4
jonathan rojas <jonathang_25@hotmail.com>
2015-10-13 15:56:24 -0430

    punto 10

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,7 +2,7 @@ class House < ActiveRecord::Base
 
 
 
-  has_attached_file :picture, :styles => {:home => '548x300>', :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture, :styles => {:home => '350x214>', :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
 
