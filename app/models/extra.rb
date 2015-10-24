b9cdf167cb1219a53d5f2697ff0fc339e46ac688
Jonathan Rojas <jonathang_25@hotmail.com>
2015-10-24 13:55:50 -0430

    se arregla la busqueda avanzada principal

--- app/models/extra.rb
+++ app/models/extra.rb
@@ -108,7 +108,8 @@ class Extra < ActiveRecord::Base
   scope :like_join, ->(str){
     self.joins("LEFT JOIN extras_brands_extras ON extras_brands_extras.extra_id = extras.id
                 LEFT JOIN brand_extras ON brand_extras.id = extras_brands_extras.brand_extra_id
-                LEFT JOIN type_trucks  ON type_trucks.id =  extras.type_truck_id").
+                LEFT JOIN types_truck_extras ON types_truck_extras.extra_id = extras.id
+                LEFT JOIN type_trucks  ON type_trucks.id =  types_truck_extras.type_truck_id").
          where("(extras.name LIKE '%#{str}%' OR
                 brand_extras.name LIKE '%#{str}%' OR
                 type_trucks.name LIKE '%#{str}%') AND
